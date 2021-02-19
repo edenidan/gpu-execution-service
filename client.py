3fea3e777da51d3994d3114ce57f167fcbb74f7f
edenidan <37079533+edenidan@users.noreply.github.com>
2021-02-19 14:55:47 +0200

    client beta version
    By Idan and Cabouly

---
+++ client.py
@@ -0,0 +1,44 @@
+import requests
+from base64 import b64encode
+import sys
+import argparse
+
+
+def post_gpu_program_to_service(server_url, program_exe):
+    data = requests.post(
+        server_url, json={"data": b64encode(program_exe).decode('ascii')})
+    if data.status_code == 200:
+        return data.text
+    raise Exception("bad response")
+
+
+def read_binary_file(path):
+    file = open(path, "rb")
+    file_data = file.read()
+    file.close()
+    return file_data
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c')
+    parser.add_argument('-e')
+    args = parser.parse_args()
+
+    if args.c and args.e:
+        raise Exception('invalid arguments')
+
+    if args.c:
+        return 'source', args.c
+    if args.e:
+        return 'binary', args.e
+
+    raise Exception('invalid arguments')
+
+
+requestPath, filePath = parse_args()
+
+gpu_program_data = read_binary_file(filePath)
+reponse = post_gpu_program_to_service(
+    f"http://localhost:3000/{requestPath}", gpu_program_data)
+print(reponse)
